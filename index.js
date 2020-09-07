00188f3c86c040ff57fa394b40e3091948d14fbc
hank-the-tank <hank.likes.matcha@gmail.com>
2020-09-07 21:54:42 +1200

    add mongoose

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express")
+const keys = require("./config/keys")
+require("./services/passport")
+const mongoose = require("mongoose")
+
+mongoose.connect(keys.mongoURI)
+
 const app = express()
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" })
-})
+require("./routes/authRoutes")(app)
 
 const PORT = process.env.PORT || 5000
-
 app.listen(PORT)
