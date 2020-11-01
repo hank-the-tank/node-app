825bbb37b04ec5704e2235b7b80fa0b21125ee0b
Hank <hank@Hanks-MBP.fritz.box>
2020-11-01 20:37:33 +1300

    impolment survey

---
+++ config/dev.js
@@ -0,0 +1,15 @@
+module.exports = {
+  googleClientID:
+    "373816001937-f5v7j7a1fhq1dn1v9cjmjgbkqqt489uk.apps.googleusercontent.com",
+  googleClientSecret: "ztWSWR2qbtV4pU_vdEVfAlqE",
+  mongoURI:
+    "mongodb+srv://hank:rpg90930@the-good-registry.zbmls.mongodb.net/the-good-registry?retryWrites=true&w=majority",
+  stripePublishableKey:
+    "pk_test_51HaWgkINwGJxqgjy2nyMhz6OOuu3moXPsPCbzBcITnf3x48w2iqxJZiwTtrIdACKkG40B1FNU8kPvci5QHQoYx2o00mQSZi19q",
+  stripeSecretKey:
+    "sk_test_51HaWgkINwGJxqgjyIg4SBH7B6kBLaEBJ9s5USCpF3IO6aQTgTeGRUjJh1ueQ84czu2LLPKGHpRERvwwFxFvtQQYr00mpF0Nuwb",
+  cookieKey: "HANKISAWESOME",
+  sendGridKey:
+    "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",
+  redirectDomain: "https://localhost:3000",
+}
